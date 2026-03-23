28837204e19511e6ad7ccd332eea1d71f1c6ab14
Paulo <paulorobertouri@gmail.com>
2026-03-23 15:20:43 -0300

    ci: run tests with coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,13 @@ export default defineConfig({
   test: {
     environment: 'node',
     globals: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80
+    }
   },
 });
